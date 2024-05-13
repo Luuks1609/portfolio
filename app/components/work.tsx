15e8257736774b65c15591d06e7dcaf2b6eb3c01
luuks1609 <luuks1609@gmail.com>
2024-05-14 01:03:11 +0200

    changes

--- app/components/work.tsx
+++ app/components/work.tsx
@@ -9,53 +9,91 @@ import "swiper/css/pagination";
 
 // import required modules
 import { Autoplay, FreeMode, Pagination } from "swiper/modules";
-import { Image } from "@nextui-org/image";
+import Image from "next/image";
 
 const slides = [
-  {
-    url: "https://nextui-docs-v2.vercel.app/images/fruit-1.jpeg",
-    alt: "Fruit",
-  },
-  {
-    url: "https://nextui-docs-v2.vercel.app/images/fruit-1.jpeg",
-    alt: "Fruit",
-  },
-  {
-    url: "https://nextui-docs-v2.vercel.app/images/fruit-1.jpeg",
-    alt: "Fruit",
-  },
-  {
-    url: "https://nextui-docs-v2.vercel.app/images/fruit-1.jpeg",
-    alt: "Fruit",
-  },
+	{
+		img: "/screenshots/koningsvogel.svg",
+		alt: "Koningsvogel",
+	},
+	{
+		img: "/screenshots/dronesecurity.svg",
+		alt: "DroneSecurity",
+		url: "https://dronesecurity.nl",
+	},
+
+	{
+		img: "/screenshots/isospect.svg",
+		alt: "Isospect",
+		url: "https://isospect.nl",
+	},
+	{
+		img: "/screenshots/isospect-portal.svg",
+		alt: "Isospect Portal",
+	},
+	{
+		img: "/screenshots/maisonlepuy.svg",
+		alt: "MaisonlePuy",
+		url: "https://maisonlepuy.nl",
+	},
+	{
+		img: "/screenshots/qrspot.svg",
+		alt: "QRSpot",
+	},
+	{
+		img: "/screenshots/qrspot-app.svg",
+		alt: "QRSpot App",
+	},
+	{
+		img: "/screenshots/loveforpets.svg",
+		alt: "Love For Pets",
+		url: "https://loveforpets.nl",
+	},
+
+	{
+		img: "/screenshots/antivlam-urenregistratie.svg",
+		alt: "Antivlam Urenregistratie",
+	},
 ];
 
 export default function Work() {
-  return (
-    <Swiper
-      slidesPerView={"auto"}
-      spaceBetween={30}
-      freeMode={true}
-      loop={true}
-      autoplay={{
-        delay: 2500,
-        disableOnInteraction: false,
-      }}
-      pagination={{
-        clickable: true,
-      }}
-      modules={[Autoplay, FreeMode, Pagination]}
-      className=""
-    >
-      {slides.map((slide, index) => (
-        <SwiperSlide key={index} className="">
-          <Image
-            alt={slide.alt}
-            src={slide.url}
-            className="hover:-translate-y-4"
-          />
-        </SwiperSlide>
-      ))}
-    </Swiper>
-  );
+	return (
+		<Swiper
+			slidesPerView={"auto"}
+			spaceBetween={30}
+			autoplay={{
+				delay: 2500,
+				disableOnInteraction: false,
+			}}
+			pagination={{
+				clickable: true,
+			}}
+			modules={[Autoplay, FreeMode, Pagination]}
+			className=""
+		>
+			{slides.map((slide, index) => (
+				<SwiperSlide key={index}>
+					{slide.url ? (
+						<a href={slide.url} target="_blank">
+							<Image
+								alt="Card background"
+								className="w-full h-full object-cover rounded-xl hover:-translate-y-4 duration-300"
+								src={slide.img}
+								height={100}
+								width={1000}
+							/>
+						</a>
+					) : (
+						<Image
+							alt="Card background"
+							className="w-full h-full object-cover rounded-xl"
+							src={slide.img}
+							height={100}
+							width={1000}
+						/>
+					)}
+				</SwiperSlide>
+			))}
+		</Swiper>
+	);
 }
