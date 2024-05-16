3822226917f2c2cea5b0bec46cbce0745f66232d
luuks1609 <luuks1609@gmail.com>
2024-05-17 00:01:54 +0200

    update image :)

--- app/components/greeting.tsx
+++ app/components/greeting.tsx
@@ -1,21 +1,21 @@
 import { Avatar } from "@nextui-org/react";
 
 export default function Greeting() {
-  return (
-    <div className="block md:flex md:flex-row-reverse md:justify-between gap-x-10 py-4">
-      <div className="mb-8 md:mb-0">
-        <Avatar src="/me.webp" className="w-40 h-40 text-large basis-1/3" />
-      </div>
-      <div className="basis-2/3">
-        <h1 className="mb-8 text-2xl font-medium tracking-tighter">
-          hey, I&apos;m lucas 👋
-        </h1>
-        <p className="">
-          I&apos;d like to describe myself as a passionate and entrepreneurial
-          (frontend) developer. My work ethic is hardworking and goal-oriented.
-          I strive to keep developing my skills and to take on new challenges.
-        </p>
-      </div>
-    </div>
-  );
+	return (
+		<div className="block md:flex md:flex-row-reverse md:justify-between gap-x-10 py-4">
+			<div className="mb-8 md:mb-0">
+				<Avatar src="/me2.webp" className="w-52 h-52 text-large basis-1/3" />
+			</div>
+			<div className="basis-2/3">
+				<h1 className="mb-8 text-2xl font-medium tracking-tighter">
+					hey, I&apos;m lucas 👋
+				</h1>
+				<p className="">
+					I&apos;d like to describe myself as a passionate and entrepreneurial (frontend)
+					developer. My work ethic is hardworking and goal-oriented. I strive to keep
+					developing my skills and to take on new challenges.
+				</p>
+			</div>
+		</div>
+	);
 }
