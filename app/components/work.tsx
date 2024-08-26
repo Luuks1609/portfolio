6ea4b4e3abf5988c62b72380e5ed07737acaea2d
luuks1609 <luuks1609@gmail.com>
2024-08-26 23:44:28 +0200

    new projects and visual changes

--- app/components/work.tsx
+++ app/components/work.tsx
@@ -12,91 +12,107 @@ import { Autoplay, FreeMode, Pagination } from "swiper/modules";
 import Image from "next/image";
 
 const slides = [
-	{
-		img: "/projects/koningsvogel.svg",
-		alt: "Koningsvogel",
-	},
-	{
-		img: "/projects/dronesecurity.svg",
-		alt: "DroneSecurity",
-		url: "https://dronesecurity.nl",
-	},
+  {
+    img: "/projects/koningsvogel.svg",
+    alt: "Koningsvogel",
+    url: "https://koningsvogel.vercel.app/",
+  },
+  {
+    img: "/projects/unit-a.png",
+    alt: "Unit-A",
+    url: "https://unit-a-security.vercel.app/",
+  },
+  {
+    img: "/projects/translatetunes.png",
+    alt: "TranslateTunes",
+    url: "https://translatetunes.web.app/",
+  },
+  {
+    img: "/projects/antivlam.png",
+    alt: "Antivlam",
+    url: "https://antivlam.vercel.app",
+  },
+  {
+    img: "/projects/dronesecurity.svg",
+    alt: "DroneSecurity",
+    url: "https://dronesecurity.nl",
+  },
+  {
+    img: "/projects/isospect.svg",
+    alt: "Isospect",
+    url: "https://isospect.nl",
+  },
+  {
+    img: "/projects/isospect-portal.svg",
+    alt: "Isospect Portal",
+    url: "https://portal.isospect.nl",
+  },
+  {
+    img: "/projects/maisonlepuy.svg",
+    alt: "MaisonlePuy",
+    url: "https://maisonlepuy.nl",
+  },
+  {
+    img: "/projects/qrspot.svg",
+    alt: "QRSpot",
+    url: "https://qrspot.nl",
+  },
+  {
+    img: "/projects/qrspot-ios.svg",
+    alt: "QRSpot App",
+  },
+  {
+    img: "/projects/loveforpets.svg",
+    alt: "Love For Pets",
+    url: "https://loveforpets.nl",
+  },
 
-	{
-		img: "/projects/isospect.svg",
-		alt: "Isospect",
-		url: "https://isospect.nl",
-	},
-	{
-		img: "/projects/isospect-portal.svg",
-		alt: "Isospect Portal",
-		url: "test",
-	},
-	{
-		img: "/projects/maisonlepuy.svg",
-		alt: "MaisonlePuy",
-		url: "https://maisonlepuy.nl",
-	},
-	{
-		img: "/projects/qrspot.svg",
-		alt: "QRSpot",
-	},
-	{
-		img: "/projects/qrspot-ios.svg",
-		alt: "QRSpot App",
-	},
-	{
-		img: "/projects/loveforpets.svg",
-		alt: "Love For Pets",
-		url: "https://loveforpets.nl",
-	},
-
-	{
-		img: "/projects/antivlam-urenregistratie.svg",
-		alt: "Antivlam Urenregistratie",
-	},
+  {
+    img: "/projects/antivlam-urenregistratie.svg",
+    alt: "Antivlam Urenregistratie",
+  },
 ];
 
 export default function Work() {
-	return (
-		<Swiper
-			slidesPerView={"auto"}
-			spaceBetween={30}
-			freeMode={true}
-			loop={true}
-			autoplay={{
-				delay: 2500,
-				disableOnInteraction: false,
-			}}
-			pagination={{
-				clickable: true,
-			}}
-			modules={[Autoplay, FreeMode, Pagination]}
-			className=""
-		>
-			{slides.map((slide, index) => (
-				<SwiperSlide key={index}>
-					{slide.url ? (
-						<a href={slide.url} target="_blank">
-							<Image
-								alt="Card background"
-								className="w-full h-full object-cover rounded-xl hover:-translate-y-4 duration-300"
-								src={slide.img}
-								height={100}
-								width={1000}
-							/>
-						</a>
-					) : (
-						<Image
-							alt="Card background"
-							className="w-full h-full object-cover rounded-xl"
-							src={slide.img}
-							height={100}
-							width={1000}
-						/>
-					)}
-				</SwiperSlide>
-			))}
-		</Swiper>
-	);
+  return (
+    <Swiper
+      slidesPerView={"auto"}
+      spaceBetween={30}
+      freeMode={true}
+      loop={true}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: true,
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Autoplay, FreeMode, Pagination]}
+      className=""
+    >
+      {slides.map((slide, index) => (
+        <SwiperSlide key={index}>
+          {slide.url ? (
+            <a href={slide.url} target="_blank">
+              <Image
+                alt="Card background"
+                className="w-full h-full object-cover rounded-xl hover:-translate-y-4 duration-300"
+                src={slide.img}
+                height={100}
+                width={1000}
+              />
+            </a>
+          ) : (
+            <Image
+              alt="Card background"
+              className="w-full h-full object-cover rounded-xl"
+              src={slide.img}
+              height={100}
+              width={1000}
+            />
+          )}
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,171 +2,109 @@ import Navbar from "../components/navbar";
 import { Card, CardHeader, Image } from "@nextui-org/react";
 
 const slides = [
-	{
-		img: "/projects-notext/koningsvogel.svg",
-		alt: "Koningsvogel",
-	},
-	{
-		img: "/projects-notext/dronesecurity.svg",
-		alt: "DroneSecurity",
-		url: "https://dronesecurity.nl",
-	},
-	{
-		img: "/projects-notext/isospect.svg",
-		alt: "Isospect",
-		url: "https://isospect.nl",
-	},
-	{
-		img: "/projects-notext/isospect-portal.svg",
-		alt: "Isospect Portal",
-	},
-	{
-		img: "/projects-notext/maisonlepuy.svg",
-		alt: "MaisonlePuy",
-		url: "https://maisonlepuy.nl",
-	},
-	{
-		img: "/projects-notext/qrspot-web.svg",
-		alt: "QRSpot",
-	},
-	{
-		img: "/projects-notext/qrspot.svg",
-		alt: "QRSpot App",
-	},
-	{
-		img: "/projects-notext/loveforpets.svg",
-		alt: "Love For Pets",
-		url: "https://loveforpets.nl",
-	},
-	{
-		img: "/projects-notext/antivlam.svg",
-		alt: "Antivlam Urenregistratie",
-	},
+  {
+    img: "/projects/koningsvogel.svg",
+    alt: "Koningsvogel",
+    url: "https://koningsvogel.vercel.app/",
+  },
+  {
+    img: "/projects/unit-a.png",
+    alt: "Unit-A",
+    url: "https://unit-a-security.vercel.app/",
+  },
+  {
+    img: "/projects/translatetunes.png",
+    alt: "TranslateTunes",
+    url: "https://translatetunes.web.app/",
+  },
+  {
+    img: "/projects/antivlam.png",
+    alt: "Antivlam",
+    url: "https://antivlam.vercel.app",
+  },
+  {
+    img: "/projects/dronesecurity.svg",
+    alt: "DroneSecurity",
+    url: "https://dronesecurity.nl",
+  },
+  {
+    img: "/projects/isospect.svg",
+    alt: "Isospect",
+    url: "https://isospect.nl",
+  },
+  {
+    img: "/projects/isospect-portal.svg",
+    alt: "Isospect Portal",
+    url: "https://portal.isospect.nl",
+  },
+  {
+    img: "/projects/maisonlepuy.svg",
+    alt: "MaisonlePuy",
+    url: "https://maisonlepuy.nl",
+  },
+  {
+    img: "/projects/qrspot.svg",
+    alt: "QRSpot",
+    url: "https://qrspot.nl",
+  },
+  {
+    img: "/projects/qrspot-ios.svg",
+    alt: "QRSpot App",
+  },
+  {
+    img: "/projects/loveforpets.svg",
+    alt: "Love For Pets",
+    url: "https://loveforpets.nl",
+  },
+
+  {
+    img: "/projects/antivlam-urenregistratie.svg",
+    alt: "Antivlam Urenregistratie",
+  },
 ];
 
 export default function Work() {
-	return (
-		<main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-			<Navbar />
-			<div className="">
-				<h2 className="font-medium text-2xl mb-8 tracking-tighter">my projects </h2>
-				<p className="">
-					Alongside my studies, I pursued freelance projects driven by my passion for
-					development. Here are some projects I&apos;ve had the pleasure to work on.
-				</p>
-				<hr className="my-6 border-neutral-100 dark:border-neutral-800" />
-			</div>
-			<div className="gap-2 grid grid-cols-12 grid-rows-4 gap-y-10 sm:gap-y-2">
-				<Card className="w-full h-[300px] col-span-12 sm:col-span-5 hover:-translate-y-3">
-					<CardHeader className="absolute z-10 top-1 flex-col items-start">
-						<p className="text-tiny text-black/65 ">NextJS & Sanity website</p>
-						<h4 className="text-black font-medium text-2xl">Koningsvogel.nl</h4>
-					</CardHeader>
-					<Image
-						removeWrapper
-						alt="Koningsvogel"
-						className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
-						src={slides[0].img}
-					/>
-				</Card>
-				<Card className="w-full h-[300px] col-span-12 sm:col-span-7">
-					<CardHeader className="absolute z-10 top-1 flex-col items-start">
-						<p className="text-tiny text-black/65 ">Wordpress & Divi website</p>
-						<h4 className="text-black font-medium text-2xl">Dronesecurity.nl</h4>
-					</CardHeader>
-					<Image
-						removeWrapper
-						alt="DroneSecurity"
-						className="z-0 w-full h-full object-cover"
-						src={slides[1].img}
-					/>
-				</Card>
-				<Card className="w-full h-[300px] col-span-12 sm:col-span-7">
-					<CardHeader className="absolute z-10 top-1 flex-col items-start">
-						<p className="text-tiny text-black/65 ">NextJS & Supabase webapp</p>
-						<h4 className="text-black font-medium text-2xl">Isospect Portal</h4>
-					</CardHeader>
-					<Image
-						removeWrapper
-						alt="Isospect Portal"
-						className="z-0 w-full h-full object-cover"
-						src={slides[3].img}
-					/>
-				</Card>
-				<Card className="w-full h-[300px] col-span-12 sm:col-span-5">
-					<CardHeader className="absolute z-10 top-1 flex-col items-start">
-						<p className="text-tiny text-black/65 ">NextJS & TailwindCSS website</p>
-						<h4 className="text-black font-medium text-2xl">Isospect.nl</h4>
-					</CardHeader>
-					<Image
-						removeWrapper
-						alt="Isospect"
-						className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
-						src={slides[2].img}
-					/>
-				</Card>
-
-				<Card className="col-span-12 sm:col-span-5 h-[300px]">
-					<CardHeader className="absolute z-10 top-1 flex-col !items-start">
-						<p className="text-tiny text-black/65 ">WordPress & Divi shop</p>
-						<h4 className="text-black font-medium text-2xl">Loveforpets.nl</h4>
-					</CardHeader>
-					<Image
-						removeWrapper
-						alt="Love For Pets"
-						className="z-0 w-full h-full object-cover"
-						src={slides[7].img}
-					/>
-				</Card>
-				<Card className="col-span-12 sm:col-span-7 h-[300px]">
-					<CardHeader className="absolute z-10 top-1 flex-col !items-start">
-						<p className="text-tiny text-black/65 ">WordPress booking website</p>
-						<h4 className="text-black font-medium text-2xl">Maisonlepuy.nl</h4>
-					</CardHeader>
-					<Image
-						removeWrapper
-						alt="MaisonlePuy"
-						className="z-0 w-full h-full object-cover"
-						src={slides[4].img}
-					/>
-				</Card>
-				<Card className="col-span-12 sm:col-span-4 h-[300px]">
-					<CardHeader className="absolute z-10 top-1 flex-col !items-start">
-						<p className="text-tiny text-black/65 ">ReactJS & Firebase webapp</p>
-						<h4 className="text-black font-medium text-2xl">QRSpot.nl</h4>
-					</CardHeader>
-					<Image
-						removeWrapper
-						alt="QRSpot"
-						className="z-0 w-full h-full object-cover"
-						src={slides[5].img}
-					/>
-				</Card>
-				<Card className="col-span-12 sm:col-span-4 h-[300px]">
-					<CardHeader className="absolute z-10 top-1 flex-col !items-start">
-						<p className="text-tiny text-black/65 ">React Native & Firebase</p>
-						<h4 className="text-black font-medium text-2xl">QRSpot iOS</h4>
-					</CardHeader>
-					<Image
-						removeWrapper
-						alt="Antivlam Urenregistratie"
-						className="z-0 w-full h-full object-cover"
-						src={slides[6].img}
-					/>
-				</Card>
-				<Card className="col-span-12 sm:col-span-4 h-[300px]">
-					<CardHeader className="absolute z-10 top-1 flex-col !items-start">
-						<p className="text-tiny text-black/65 ">Svelte & Firebase</p>
-						<h4 className="text-black font-medium text-2xl">Antivlam Employees</h4>
-					</CardHeader>
-					<Image
-						removeWrapper
-						alt="Antivlam Urenregistratie"
-						className="z-0 w-full h-full object-cover"
-						src={slides[8].img}
-					/>
-				</Card>
-			</div>
-		</main>
-	);
+  return (
+    <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+      <Navbar />
+      <div className="">
+        <h2 className="font-medium text-2xl mb-8 tracking-tighter">
+          my projects{" "}
+        </h2>
+        <p className="">
+          Alongside my studies, I pursued freelance projects driven by my
+          passion for development. Here are some projects I&apos;ve had the
+          pleasure to work on.
+        </p>
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+      </div>
+      <div className="gap-2 grid grid-cols-12 grid-rows-4 gap-y-10 sm:gap-y-2">
+        {slides.map((slide, index) => (
+          <a
+            href={slide.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            key={index}
+            className={`col-span-6 sm:col-span-${
+              Math.floor(index / 2) % 2 === 0 ? "4" : "4"
+            }`}
+          >
+            <Card
+              key={index}
+              className={`w-full h-[300px] col-span-12 sm:col-span-${
+                Math.floor(index / 2) % 2 === 0 ? "4" : "4"
+              } hover:-translate-y-3`}
+            >
+              <Image
+                removeWrapper
+                alt={slide.alt}
+                className="z-0 w-full hover:scale-[115%] h-full scale-110  object-cover"
+                src={slide.img}
+              />
+            </Card>
+          </a>
+        ))}
+      </div>
+    </main>
+  );
 }
