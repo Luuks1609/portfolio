b257484fc911dabcfffae970bb3caa6f974e670d
luuks1609 <luuks1609@gmail.com>
2024-09-23 15:54:03 +0200

    added an ecommerce project and rewrote some texts.

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,67 +1,6 @@
+import { slides } from "@/lib/projects";
 import Navbar from "../components/navbar";
-import { Card, CardHeader, Image } from "@nextui-org/react";
-
-const slides = [
-  {
-    img: "/projects/koningsvogel.svg",
-    alt: "Koningsvogel",
-    url: "https://koningsvogel.vercel.app/",
-  },
-  {
-    img: "/projects/unit-a.png",
-    alt: "Unit-A",
-    url: "https://unit-a-security.vercel.app/",
-  },
-  {
-    img: "/projects/translatetunes.png",
-    alt: "TranslateTunes",
-    url: "https://translatetunes.web.app/",
-  },
-  {
-    img: "/projects/antivlam.png",
-    alt: "Antivlam",
-    url: "https://antivlam.vercel.app",
-  },
-  {
-    img: "/projects/dronesecurity.svg",
-    alt: "DroneSecurity",
-    url: "https://dronesecurity.nl",
-  },
-  {
-    img: "/projects/isospect.svg",
-    alt: "Isospect",
-    url: "https://isospect.nl",
-  },
-  {
-    img: "/projects/isospect-portal.svg",
-    alt: "Isospect Portal",
-    url: "https://portal.isospect.nl",
-  },
-  {
-    img: "/projects/maisonlepuy.svg",
-    alt: "MaisonlePuy",
-    url: "https://maisonlepuy.nl",
-  },
-  {
-    img: "/projects/qrspot.svg",
-    alt: "QRSpot",
-    url: "https://qrspot.nl",
-  },
-  {
-    img: "/projects/qrspot-ios.svg",
-    alt: "QRSpot App",
-  },
-  {
-    img: "/projects/loveforpets.svg",
-    alt: "Love For Pets",
-    url: "https://loveforpets.nl",
-  },
-
-  {
-    img: "/projects/antivlam-urenregistratie.svg",
-    alt: "Antivlam Urenregistratie",
-  },
-];
+import { Card, Image } from "@nextui-org/react";
 
 export default function Work() {
   return (
---
+++ lib/projects.ts
@@ -0,0 +1,65 @@
+export const slides = [
+  {
+    img: "/projects/koningsvogel.svg",
+    alt: "Koningsvogel",
+    url: "https://koningsvogel.vercel.app/",
+  },
+  {
+    img: "/projects/unit-a.png",
+    alt: "Unit-A",
+    url: "https://unit-a-security.vercel.app/",
+  },
+  {
+    img: "/projects/translatetunes.png",
+    alt: "TranslateTunes",
+    url: "https://translatetunes.web.app/",
+  },
+  {
+    img: "/projects/antivlam.png",
+    alt: "Antivlam",
+    url: "https://antivlam.vercel.app",
+  },
+  {
+    img: "/projects/dronesecurity.svg",
+    alt: "DroneSecurity",
+    url: "https://dronesecurity.nl",
+  },
+  {
+    img: "/projects/isospect.svg",
+    alt: "Isospect",
+    url: "https://isospect.nl",
+  },
+  {
+    img: "/projects/isospect-portal.svg",
+    alt: "Isospect Portal",
+    url: "https://portal.isospect.nl",
+  },
+  {
+    img: "/projects/maisonlepuy.svg",
+    alt: "MaisonlePuy",
+    url: "https://maisonlepuy.nl",
+  },
+  {
+    img: "/projects/qrspot.svg",
+    alt: "QRSpot",
+    url: "https://qrspot.nl",
+  },
+  {
+    img: "/projects/qrspot-ios.svg",
+    alt: "QRSpot App",
+  },
+  {
+    img: "/projects/loveforpets.svg",
+    alt: "Love For Pets",
+    url: "https://loveforpets.nl",
+  },
+  {
+    img: "/projects/antivlam-urenregistratie.svg",
+    alt: "Antivlam Urenregistratie",
+  },
+  {
+    img: "/projects/floatingsneaker.png",
+    alt: "FloatingSneaker",
+    url: "https://floatingsneaker.com",
+  },
+];
